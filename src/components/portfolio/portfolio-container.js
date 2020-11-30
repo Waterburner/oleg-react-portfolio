22428d3eb953cc6db2cd91f65ae48bd724854891
Waterburner <Waterburner@users.noreply.github.com>
2020-11-30 01:30:49 -0600

    Added buttons, handleFilter, workflow, and state "isLoading"
    Added buttons, handleFilter, workflow, and state "isLoading"

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,79 @@
+import React, {Component} from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            isLoading: true,                        // THIS IS JUST A TEST
+            data: [
+                {title: "title1", category: "eCommerce"},
+                {title: "title2", category: "Scheduling"},
+                {title: "title3", category: "Enterprise"},
+                {title: "title4", category: "eCommerce"},
+                
+            ]
+        };
+
+        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+        this.undoHandlePageTitle = this.undoHandlePageTitle.bind(this);
+
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+handleFilter(filter) {
+    this.setState({
+        data: this.state.data.filter(item => {
+            return item.category === filter;
+        })
+    })
+}
+
+    portfolioItems() {
+
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"url_hard_coded"}/>;
+        })
+    }
+
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "Something else"
+        });
+    }
+
+    undoHandlePageTitle() {
+        this.setState({
+            pageTitle: "Welcome to my portfolio"
+        });
+    }
+
+    render() {
+        if (this.state.isLoading) { 
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+                {this.portfolioItems()}
+
+                <hr/>
+                <button onClick={this.handlePageTitleUpdate}>Change title</button>
+                <button onClick={this.undoHandlePageTitle}>Undo change title</button>
+
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button onClick={() => this.handleFilter('Enerprise')}>Enterprise</button>
+
+                
+
+                
+            </div>
+        )
+    }
+}
