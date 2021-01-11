a89107cadcb88260d80954280f75d49a926f158c
Waterburner <Waterburner@users.noreply.github.com>
2021-01-11 17:37:23 -0600

    added styles at portfolio.scss. Still needs finishing touch and hovering shading effect etc

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -3,18 +3,27 @@ import { Link } from "react-router-dom";
 
 export default function(props) {
     //data that we'll need:
-    // - background pic thumb_image_url
-    // - logo
+    // - background pic: thumb_image_url
+    // - logo: logo_url
     // - description: description
     // - id: id
 
-      const { id, description, thumb_image_url, logo} = props.item;
+      const { id, description, thumb_image_url, logo_url} = props.item;
     return (
     <div className="portfolio-item-wrapper">
-      <img src={thumb_image_url} />
-      <img src={logo} />
-      <div>{description}</div>
-      <Link to={`/portfolio/${id}`}>Link</Link>
+
+      <div 
+        className="portfolio-img-background"
+        style={{backgroundImage: "url(" + thumb_image_url + ")"}}
+      />
+
+      <div className="img-text-wrapper">
+        <div className="logo-wrapper">
+          <img src={logo_url} />
+        </div>
+
+        <div className="subtitle">{description}</div>
+      </div>
     </div>
   );
 }
