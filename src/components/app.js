e7674dea47485b3831f91f00051a755ee803f03a
Waterburner <Waterburner@users.noreply.github.com>
2021-06-22 22:28:11 -0500

    Created blog-detail.js and new folder "blog" in component folder. Working on dynamic blog components

--- src/components/app.js
+++ src/components/app.js
@@ -17,6 +17,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -133,6 +134,8 @@ export default class App extends Component {
             <Route path="/about-me" component ={About} />
             {/* <Route path="/test" component ={test} /> don't forget about navigation-container */}
             <Route path="/blog" component ={Blog} />
+            <Route path="/b/:slug" component ={BlogDetail} />
+
             <Route path="/contact" component ={Contact} />
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
             <Route exact path="/portfolio/:slug" component ={PortfolioDetail} />
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import BlogItem from '../blog/blog-item';
 
 export default class Blog extends Component {
     constructor() {
         super();
+
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
     }
 
+    getBlogItems() {
+        axios.get("https://waterburner.devcamp.space/portfolio/portfolio_blogs", {
+                withCredentials: true
+            })
+            .then(response => {
+
+                // console.log('response', response); // this is how we see response.data.portfolio_blogs
+                
+
+                this.setState({
+                    blogItems: response.data.portfolio_blogs
+                });
+            })
+            .catch(error => {
+                console.log('getBlogItems error', error);
+            });
+    }
+
+    UNSAFE_componentWillMount() {
+        this.getBlogItems();
+    }
 
     render() {
 
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <BlogItem key={blogItem.id} blogItem={blogItem} />
+        });
+
         return (
             <div>
-            <h2>Blog</h2>
-
-            <div>
-                <Link to="/about-me">Read more about myself</Link>
+                {blogRecords}
             </div>
-        </div>
     );
     }
 }
