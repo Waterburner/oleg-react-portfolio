267eca0662f0281a0f14d7f87d9ff8e1aaf99877
Waterburner <Waterburner@users.noreply.github.com>
2021-06-25 17:23:41 -0500

    Changed activateInfiniteScroll in "blog.js" to check if we reached to the bottom of the page

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -12,6 +12,17 @@ export default class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
+
+        this.activateInfiniteScroll();
+    }
+
+    activateInfiniteScroll() {
+        window.onscroll = () => {
+
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+                console.log('get more posts');
+            }
+        }
     }
 
     getBlogItems() {
