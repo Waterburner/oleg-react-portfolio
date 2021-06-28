b164b28502529fcfa3657da868ab7dd3786eeffc
Waterburner <Waterburner@users.noreply.github.com>
2021-06-28 15:13:49 -0500

    Created handleSubmit in blog-form.js with axios. Now we can actually submit new blog post from form component

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class BlogForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            blog_status: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_blog[title]", this.state.title);
+    formData.append("portfolio_blog[status]", this.state.blog_status);
+
+    return formData;
+}
+
+handleSubmit(event) {
+    axios.post(
+        "https://waterburner.devcamp.space/portfolio/portfolio_blogs",
+        this.buildForm(), 
+        {withCredentials: true}
+    ).then(response => {
+        this.props.handleSuccessfullFormSubmission(response.data);
+    }).catch(error => {
+        console.log("handleSubmit for blog error", error);
+    });
+
+    event.preventDefault();
+}
+
+handleChange(event) {
+    this.setState({
+        [event.target.name]: event.target.value
+    });
+}
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        onChange={this.handleChange}
+                        name="title"
+                        placeholder="Blog Title"
+                        value={this.state.title}
+                    />
+                    <input 
+                        type="text" 
+                        onChange={this.handleChange}
+                        name="blog_status"
+                        placeholder="Blog Status"
+                        value={this.state.blog_status}
+                    />
+
+                    <button>Save</button>
+                </form>
+                
+            </div>
+        );
+    }
+}
